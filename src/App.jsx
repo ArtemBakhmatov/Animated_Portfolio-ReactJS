55a509d2b04ca40ce4d287839dc4e9e2051af722
artem_bakhmatov <ip.bakhmatov@mail.ru>
2023-10-17 17:24:18 +0300

    Navbar Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import Navbar from './components/navbar/Navbar';
+
 import './app.scss';
 
 const App = () => {
 	return <div>
-		<section><a href="">Hero</a></section>
+		<section>
+			<Navbar />
+		</section>
 		<section>Parallax</section>
 		<section>Services</section>
 		<section>Parallax</section>
